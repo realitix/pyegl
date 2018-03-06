cac6ecbd0989694f0244602bb9cd2eb60db5b723
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-03-06 09:00:41 +0100

    Include egl

---
+++ pyegl/pyegl_build/pyegl_build.py
@@ -0,0 +1,41 @@
+from cffi import FFI
+from os import path
+import platform
+
+
+HERE = path.dirname(path.realpath(__file__))
+INCLUDE_FOLDER = path.join(HERE, 'include')
+LINUX = platform.system() == 'Linux'
+
+# ----------
+# BUILD WRAPPER
+# ----------
+ffibuilder = FFI()
+
+# prepare cdef
+cdef = ""
+#if LINUX:
+#    cdef += open(path.join(HERE, 'cdef', 'stddef.cdef.h')).read()
+cdef += open(path.join(HERE, 'cdef', 'egl.cdef.h')).read()
+
+ffibuilder.cdef(cdef)
+
+
+# prepare libraries
+libs = ['vk_mem_alloc']
+
+# prepare source
+source = '''
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+'''
+
+ffibuilder.set_source(
+    '_pyegl',
+    source,
+    extra_compile_args=["-I"+INCLUDE_FOLDER]
+)
+
+
+if __name__ == '__main__':
+    ffibuilder.compile(verbose=True)
