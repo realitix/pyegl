9869c614c0a19ec5aa7149972f43f9a1b5d62f60
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-03-25 18:53:20 +0200

    Update egl

--- pyegl/__init__.py
+++ pyegl/__init__.py
@@ -3,7 +3,23 @@ from pyegl._pyegl import ffi, lib
 
 EGL_DEFAULT_DISPLAY = ffi.cast('EGLNativeDisplayType', 0)
 EGL_NO_DISPLAY = ffi.cast('EGLDisplay', 0)
+EGL_NO_SURFACE = ffi.cast('EGLSurface', 0)
+
+
+class EglError(Exception):
+    pass
 
 
 def eglGetDisplay(display_id):
     return lib.eglGetDisplay(display_id)
+
+
+def eglInitialize(dpy):
+    major = ffi.new('EGLint*')
+    minor = ffi.new('EGLint*')
+    result = lib.eglInitialize(dpy, major, minor)
+
+    if not result:
+        raise EglError("Can't initialize display")
+
+    return major[0], minor[0]
--- pyegl/pyegl_build/cdef/egl.cdef.h
+++ pyegl/pyegl_build/cdef/egl.cdef.h
@@ -1,5 +1,10 @@
 typedef void* EGLDisplay;
 typedef void* EGLNativeDisplayType;
+typedef unsigned int EGLBoolean;
+typedef int32_t khronos_int32_t;
+typedef khronos_int32_t EGLint;
+typedef void* EGLSurface;
 
 
 EGLDisplay eglGetDisplay(EGLNativeDisplayType display_id);
+EGLBoolean eglInitialize(EGLDisplay dpy, EGLint *major, EGLint *minor);
