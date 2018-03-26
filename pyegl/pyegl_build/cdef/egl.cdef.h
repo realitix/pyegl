701cfc1f8570dc9abf838504e2220f9426d8c229
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-03-26 14:33:17 +0200

    Add al constants

--- pyegl/pyegl_build/cdef/egl.cdef.h
+++ pyegl/pyegl_build/cdef/egl.cdef.h
@@ -4,7 +4,13 @@ typedef unsigned int EGLBoolean;
 typedef int32_t khronos_int32_t;
 typedef khronos_int32_t EGLint;
 typedef void* EGLSurface;
+typedef void* EGLConfig;
+typedef void *EGLContext;
+typedef void *EGLSync;
+typedef void *EGLImage;
 
 
 EGLDisplay eglGetDisplay(EGLNativeDisplayType display_id);
-EGLBoolean eglInitialize(EGLDisplay dpy, EGLint *major, EGLint *minor);
+EGLBoolean eglInitialize(EGLDisplay display, EGLint *major, EGLint *minor);
+EGLBoolean eglGetConfigs(EGLDisplay display, EGLConfig *configs, EGLint config_size, EGLint *num_config);
+EGLBoolean eglChooseConfig (EGLDisplay dpy, const EGLint *attrib_list, EGLConfig *configs, EGLint config_size, EGLint *num_config);
